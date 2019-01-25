bc3ef9cd48874c33e48efd2fc8df6e01c1b0526b
andrew blinn <me@disconcision.com>
2019-01-24 19:48:18 -0500

    simple counter

--- haskell/SPL/test/VList_test.hs
+++ haskell/SPL/test/VList_test.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE BangPatterns #-}
 
 import VList
 import SPL
@@ -9,6 +10,7 @@ import ShareVis
 
 import Control.Monad.State.Lazy
 import Data.Tuple
+import Data.List
 
 _p_, _q_, _r_, _s_ :: (String, Prop)
 univ@[_p_, _q_, _r_, _s_] = mkUniverse ["P", "Q", "R", "S"]
@@ -35,10 +37,12 @@ l2 = vCons x l1
 l3 = vCons y l2
 l4 = vCons z l3
 
-listBegin = mkVList[y, z, x, w]
-listEnd = mkVList[w,x,z,y]
-listMiddle = mkVList [x, y, z, w]
+listEnd = mkVList[y,y,y,y]
+listEnd2 = mkVList[w,y,y,y]
+listEnd3 = mkVList[w,z,y,y]
 
+listMiddle = mkVList [x, y, z, w]
+listBegin = mkVList[y, y, y, w]
 
 -- let's figure out these types. we know
 
@@ -67,25 +71,71 @@ varToList (Var []) = []
 varToList (Var ((t,prop):xs)) = t : (varToList (Var xs)) 
 
 
-countOps :: VList a -> Var (State Int Int)
-countOps vl = vfoldr (mkVarT f) (mkVarT base) vl where
-    base = (get >>= return) :: State Int Int
-    f :: a -> (State Int Int) -> (State Int Int)
+
+-- cond' :: Show a => Var Bool -> Var a -> Var a -> Var a
+-- vhead :: VList a -> Var a 
+-- vtail :: VList a -> VList a 
+-- vnull :: VList a -> Var Bool
+
+
+vfoldrDeep :: Show a => Var (b -> a -> a) -> Var a -> VList b -> Var a
+vfoldrDeep f init ls = cond' (vnull ls)
+    init
+    $ f <*> vhead ls <*> vfoldrDeep f init (vtail ls)
+
+    -- because cond' <= Show
+instance Show (State a b) where
+    show _ = "" -- (StateT f)
+
+
+
+
+vCounter :: Show a => VList a -> Int
+vCounter vls =
+    let xs = case vls of Var ls -> fmap (\(a,b) -> a) ls
+        mm = map getName xs
+    in
+        if null (head xs)
+            then 0 
+            else (length $ nub $ mm) + (vCounter $ vtail vls)
+
+
+
+countOps ::  VList a -> Var (State [Int] [Int])
+countOps vl = vfoldrDeep (mkVarT f) (mkVarT base) vl where
+    base = (get >>= return) :: State [Int] [Int]
+    f :: a -> (State [Int] [Int]) -> (State [Int] [Int])
     f x state = do
-        count <- get
-        put (count+1)
+        seenList <- get
+        put (getName x : seenList)
         state
 
 
-countAcrossVar :: VList a -> Int
-countAcrossVar vl = sum(varToList vi)
-    where vi = (liftV2 evalState) (countOps vl) (mkVarT 0)
+getNames :: VList a -> Var [Int]
+getNames vl = vfoldrDeep (mkVarT f) (mkVarT []) vl
+        where
+            f x names = (getName x) : names
+
+--countAcrossVar :: VList a -> Int
+--countAcrossVar vl = sum(varToList vi)
+--    where vi = (liftV2 evalState) (countOps vl) (mkVarT 0)
 
 
 graphVList (Var ls) = showGraph $ fmap (\(a,b)-> (show b,a)) ls
 
+realCount (Var ls) =    fmap (\(a,b)-> a) ls
+-- sum $ map length $ map nub $ transpose $
+
 
 main = do
-    graphVList l4
+    print $ vCounter listEnd
+    print $ vCounter listEnd2
+    print $ vCounter listEnd3
+    print $ getNames listEnd
+    print $ case listEnd of Var ls -> fmap (\(a,b)-> (show b,a)) ls
+    graphVList $ listEnd
     --print $ vfoldr (mkVarT (+)) (mkVarT 0) l3
-    print $ countAcrossVar l4
+    print $ (liftV2 evalState) (countOps listEnd) (mkVarT [])
+    print $ realCount $ (liftV2 evalState) (countOps listEnd) (mkVarT [])
+    --print $ countAcrossVar listEnd
+    
